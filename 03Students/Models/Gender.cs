c8c2d87178e246c76a0c26bd02b7be455b30744e
AleksandraAntonijevic <149418568+AleksandraAntonijevic@users.noreply.github.com>
2024-04-02 15:00:34 +0200

    new

--- 03Students/Models/Gender.cs
+++
@@ -1,6 +0,0 @@
-// 03Students.Models.Gender.cs
-
-namespace _03Students.Models
-{
-    public enum Gender { Male, Female }
-}
--- 03Students/Models/Subject.cs
+++
@@ -1,9 +0,0 @@
-// 03Students.Models.Subject.cs
-
-namespace _03Students.Models
-{
-    public class Subject
-    {
-        public string Name { get; set; }
-    }
-}
